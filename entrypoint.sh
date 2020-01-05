4abcd10b0c30388a0866b88cc9d52390f0dc38fc
Aaron Wislang <aaron.wislang@microsoft.com>
2020-01-04 23:17:40 -0500

    fix message and exit on failure

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+set -euo pipefail
 
 # default values
 [[ -z "$SESSION_CODE" ]] && export SESSION_CODE='TWT'
 # supplied via docker run:
-[[ ! -z "$RESOURCE_GROUP_NAME" ]] && echo "RESOURCE_GROUP_NAME not set!"
-[[ ! -z "$TENANT_ID" ]] && echo "TENANT_ID not set!"
-[[ ! -z "$CLIENT_ID" ]] && echo "CLIENT_ID not set!"
-[[ ! -z "$CLIENT_SECRET" ]] && echo "CLIENT_SECRET not set!"
-[[ ! -z "$USER_ID" ]] && echo "USER_ID not set!"
+[[ -z "$RESOURCE_GROUP_NAME" ]] && echo "RESOURCE_GROUP_NAME not set!" && exit 1
+[[ -z "$TENANT_ID" ]] && echo "TENANT_ID not set!" && exit 1
+[[ -z "$CLIENT_ID" ]] && echo "CLIENT_ID not set!" && exit 1
+[[ -z "$CLIENT_SECRET" ]] && echo "CLIENT_SECRET not set!" && exit 1
+[[ -z "$USER_ID" ]] && echo "USER_ID not set!" && exit 1
 
 # Log into Azure with Service Principal
 az login --service-principal --tenant $TENANT_ID --username $CLIENT_ID --password $CLIENT_SECRET 
