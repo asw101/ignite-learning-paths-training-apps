4ddfe6a5f009d649d5d00feadffef801d374f4f5
Aaron Wislang <aaron.wislang@microsoft.com>
2020-01-04 22:46:03 -0500

    entrypoint.sh at root

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 # default values
-[[ -z "$SOURCE_REPOSITORY" ]] && export SOURCE_REPOSITORY='https://github.com/microsoft/ignite-learning-paths-training-apps.git'
-[[ -z "$SOURCE_BRANCH" ]] && export SOURCE_BRANCH='master'
-[[ -z "$ENTRYPOINT" ]] && export ENTRYPOINT='/apps40/deploy.sh'
 [[ -z "$SESSION_CODE" ]] && export SESSION_CODE='TWT'
 # supplied via docker run:
 [[ ! -z "$RESOURCE_GROUP_NAME" ]] && echo "RESOURCE_GROUP_NAME not set!"
@@ -18,12 +15,4 @@ az login --service-principal --tenant $TENANT_ID --username $CLIENT_ID --passwor
 # Tag Resource Group
 az group update --resource-group $RESOURCE_GROUP_NAME --tags ITT-Tracking=$SESSION_CODE CreatedBy=$USER_ID
 
-# Get scripts and deployment assets
-git clone -branch $SOURCE_BRANCH $SOURCE_REPOSITORY
-
-# Get directory
-GIT=${SOURCE_REPOSITORY##*/}
-DIR=${GIT%.*}
-
-# Run scripts
-bash $DIR$ENTRYPOINT
+bash apps40/deploy.sh
