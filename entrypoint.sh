f86f60c942eeab8845db36f4b3765c931be14ac2
Aaron Wislang <aaron.wislang@microsoft.com>
2020-01-04 23:18:44 -0500

    set correct directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,4 +16,5 @@ az login --service-principal --tenant $TENANT_ID --username $CLIENT_ID --passwor
 # Tag Resource Group
 az group update --resource-group $RESOURCE_GROUP_NAME --tags ITT-Tracking=$SESSION_CODE CreatedBy=$USER_ID
 
-bash apps40/deploy.sh
+cd apps40/
+bash deploy.sh
